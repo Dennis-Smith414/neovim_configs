981bea1e241a8c280048cf803c57c478d90b8f79
Dennis-Smith414 <dennisnsmith2000@gmail.com>
2025-12-18 03:27:11 +0000

    save commands

--- init.lua
+++ init.lua
@@ -5,10 +5,28 @@ vim.g.mapleader = " "
 -- Set keyboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Disable swap files
+vim.opt.swapfile = false
+
+
 --KEY BINDS
+
+--Copy and paste controls
 vim.keymap.set("v", "<C-c>", '"+y')
 vim.keymap.set({"n", "v"}, "<C-y>", '"+yy')
 vim.keymap.set("n", "<C-p>", '"+p')
 vim.keymap.set("i", "<C-p>", '<C-r>+')
 vim.keymap.set("i", "<C-y>", '<Esc>"+yygi') 
+
+--Save
+vim.keymap.set("n", "<C-s>", ":w<cr>")
+vim.keymap.del("i", "<C-s>")
+vim.keymap.set("i", "<C-s>", '<Esc>:w<CR>:echo "File saved"<CR>a', { noremap = true })
+--Save and exit
+vim.keymap.set("n", "<C-o>", ":wq<cr>")
+vim.keymap.set("n", "<C-k>", ":q!<cr>") --exit without saving
+
+
+
+--If youre seeing this I saved in insert!
 
